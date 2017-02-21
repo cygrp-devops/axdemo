07014d8468945fd6a194fc82adea5935f2c878ea
Taha Uddin <taha.uddin@cygrp.com>
2017-02-21 15:39:50 -0600

    changes ps1 to reflect right rg. removed crap from web.params

--- Deploy-AzureResourceGroup.ps1
+++ Deploy-AzureResourceGroup.ps1
@@ -1,2 +1,2 @@
 New-AzureRmResourceGroup -Name ExampleResourceGroup -Location "South Central US"
-New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleResourceGroup -TemplateFile c:\MyTemplates\example.json -TemplateParameterFile c:\MyTemplates\example.params.json
+New-AzureRmResourceGroupDeployment -Name ExampleDeployment -ResourceGroupName ExampleResourceGroup -TemplateFile C:\_git\Infrastructure\azurerm\AXDemo\azuredeploy.json
